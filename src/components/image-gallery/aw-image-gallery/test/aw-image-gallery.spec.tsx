a6a3df40bd25952d111fc642ead89e143cac14c1
David Enrique Prieto Melendez <david.enrique.prieto.melendez@everis.com>
2021-07-17 12:06:58 +0200

    test: fixed unit test

--- src/components/image-gallery/aw-image-gallery/test/aw-image-gallery.spec.tsx
+++ src/components/image-gallery/aw-image-gallery/test/aw-image-gallery.spec.tsx
@@ -1,18 +1,43 @@
 import { newSpecPage } from '@stencil/core/testing';
+import { AwImageItem } from '../../aw-image-item/aw-image-item';
 import { AwImageGallery } from '../aw-image-gallery';
 
 describe('aw-image-gallery', () => {
-  it('renders', async () => {
-    const page = await newSpecPage({
-      components: [AwImageGallery],
-      html: `<aw-image-gallery></aw-image-gallery>`,
+    it('renders', async () => {
+        const imgSrc = 'https://picsum.photos/200/300';
+        const page = await newSpecPage({
+            components: [AwImageGallery, AwImageItem],
+            html: `
+                <aw-image-gallery>
+                    <aw-image-item image-src="${imgSrc}"></aw-image-item>
+                </aw-image-gallery>
+            `,
+        });
+        expect(page.root).toEqualHtml(`
+            <aw-image-gallery>
+                <mock:shadow-root>
+                    <aw-grid>
+                        <aw-row class="middle-xs">
+                            <aw-col class="size-xs-12 size-sm-6 size-md-4 size-lg-4" >
+                                <aw-image-item class="image-gallery-item">
+                                    <mock:shadow-root>
+                                        <picture>
+                                            <img src="${imgSrc}"/>
+                                        </picture>
+                                    </mock:shadow-root>
+                                </aw-image-item>
+                            </aw-col>
+                        </aw-row>
+                    </aw-grid>
+                </mock:shadow-root>
+                <aw-image-item image-src="${imgSrc}">
+                    <mock:shadow-root>
+                        <picture>
+                            <img src="${imgSrc}"/>
+                        </picture>
+                    </mock:shadow-root>
+                </aw-image-item>
+            </aw-image-gallery>
+        `);
     });
-    expect(page.root).toEqualHtml(`
-      <aw-image-gallery>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </aw-image-gallery>
-    `);
-  });
 });
--- src/components/image-gallery/aw-image-item/test/aw-image-item.spec.tsx
+++ src/components/image-gallery/aw-image-item/test/aw-image-item.spec.tsx
@@ -2,17 +2,20 @@ import { newSpecPage } from '@stencil/core/testing';
 import { AwImageItem } from '../aw-image-item';
 
 describe('aw-image-item', () => {
-  it('renders', async () => {
-    const page = await newSpecPage({
-      components: [AwImageItem],
-      html: `<aw-image-item></aw-image-item>`,
+    it('renders', async () => {
+        const imgSrc = 'https://picsum.photos/200/300';
+        const page = await newSpecPage({
+            components: [AwImageItem],
+            html: `<aw-image-item image-src="${imgSrc}"></aw-image-item>`,
+        });
+        expect(page.root).toEqualHtml(`
+            <aw-image-item image-src="${imgSrc}">
+                <mock:shadow-root>
+                    <picture>
+                        <img src="${imgSrc}"/>
+                    </picture>
+                </mock:shadow-root>
+            </aw-image-item>
+        `);
     });
-    expect(page.root).toEqualHtml(`
-      <aw-image-item>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </aw-image-item>
-    `);
-  });
 });
